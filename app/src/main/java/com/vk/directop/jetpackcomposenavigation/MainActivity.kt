242e5b7156eb03fd99545b5b1f5e2fba42577cec
Pavel Dolganov mSt <fire_ra@bk.ru>
2023-11-09 13:29:01 +0300

    init commit

---
+++ app/src/main/java/com/vk/directop/jetpackcomposenavigation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.vk.directop.jetpackcomposenavigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.vk.directop.jetpackcomposenavigation.ui.theme.JetpackComposeNavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeNavigationTheme {
+
+            }
+        }
+    }
+}
